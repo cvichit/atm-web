da9ac77f04fc3a23a190494e88a2cf596720ac78
Vichit <vichit.su@rd.go.th>
2020-08-26 15:20:36 +0700

    refactor

---
+++ src/main/java/th/go/rd/atm/service/CustomerService.java
@@ -0,0 +1,25 @@
+package th.go.rd.atm.service;
+
+import org.springframework.stereotype.Service;
+import th.go.rd.atm.model.Customer;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CustomerService {
+    private List<Customer> customerList;
+
+    @PostConstruct
+    public void postConstruct(){
+        this.customerList = new ArrayList<>();
+    }
+    public void createCustomer(Customer customer){
+        // ..... hash pin .....
+        customerList.add(customer);
+    }
+    public List<Customer> getCustomerList(){
+        return new ArrayList<>(this.customerList);
+    }
+}
